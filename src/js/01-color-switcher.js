a5515b32f8e448f6ae4e77862ec34dede9002468
JuliaDzimina <132845748+JuliaDzimina@users.noreply.github.com>
2023-10-27 11:05:16 +0300

    task01

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,30 @@
+const  selectors = {
+    startBtn: document.querySelector('button[data-start]'),
+    stopBtn: document.querySelector('button[data-stop]'),
+    conteiner: document.querySelector('body'),
+}
+let intervalId = null;
+
+function getRandomHexColor() {
+return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+function startChangingColor() {
+    // disabled button Start
+    selectors.startBtn.disabled = true; 
+
+    intervalId = setInterval(() => {
+    selectors.conteiner.style.backgroundColor = getRandomHexColor();
+    }, 1000);
+    }
+
+
+function stopChangingColor() {
+  //active button Start
+  selectors.startBtn.disabled = false; 
+  clearInterval(intervalId);
+}
+
+selectors.startBtn.addEventListener('click', startChangingColor);
+selectors.stopBtn.addEventListener('click', stopChangingColor);
 
